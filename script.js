76bb97797a438c5a868067812e542339b84b1700
Rafaella2403 <rribeirolimabritto@gmail.com>
2022-04-01 19:52:58 -0300

    Configurando scroll suave

--- script.js
+++ script.js
@@ -42,6 +42,36 @@ function accordionNav(){
     });
 }
 
+//Configurando o scroll suave para os links internos
+
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  
+    function scrollToSection(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute('href');
+      const section = document.querySelector(href);
+      section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+  
+      // forma alternativa
+      // const topo = section.offsetTop;
+      // window.scrollTo({
+      //   top: topo,
+      //   behavior: 'smooth',
+      // });
+    }
+  
+    linksInternos.forEach((link) => {
+      link.addEventListener('click', scrollToSection);
+    });
+  }
+
+
+
 //Ativando funções
 navegacaoTab();
 accordionNav();
+initScrollSuave();
