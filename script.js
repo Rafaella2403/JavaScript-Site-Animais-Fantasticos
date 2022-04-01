1f592d6a0cd7d7b3dc2fff02d98a3c8956103c86
Rafaella2403 <rribeirolimabritto@gmail.com>
2022-04-01 19:29:51 -0300

    Configurando accordion nav - CSS

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 //Criando o processo de navegação por tabs
 function navegacaoTab(){
+    const ativoClass = 'ativo';
     const tabMenu = document.querySelectorAll('.js-tabmenu li');
     const tabDescricao = document.querySelectorAll('.js-tabdescricao section');
 
     //Verificando se os itens existe na tela
 
     if(tabMenu.length > 0 && tabDescricao.length > 0){
-        tabDescricao[0].classList.add('ativo');
+        tabDescricao[0].classList.add(ativoClass);
 
         //Função responsável por adicionar a classe ativo ao elemento selecionar e remover dos demais elementos
         function ativarTab(index){
             tabDescricao.forEach(section => {
-                section.classList.remove('ativo');
+                section.classList.remove(ativoClass);
             });
-            tabDescricao[index].classList.add('ativo');
+            tabDescricao[index].classList.add(ativoClass);
         }
 
         tabMenu.forEach((itemMenu, index) => {
@@ -24,16 +25,23 @@ function navegacaoTab(){
     }
 }
 
-//Ativando função de navegação por tab
-navegacaoTab();
+//Configurando o accodion nav
+function accordionNav(){ 
+    const ativoClass = 'ativo';
+    const accordionLista = document.querySelectorAll('.js-accordion dt');
+    accordionLista[0].classList.add(ativoClass);
+    accordionLista[0].nextElementSibling.classList.toggle(ativoClass);
 
-const accordionLita = document.querySelectorAll('.js-accordion');
+    function ativarArccodion(){
+        this.classList.toggle(ativoClass);
+        this.nextElementSibling.classList.toggle(ativoClass);
+    }
 
-function ativarArccodion(){
-    this.classList.add('ativo');
-    this.nextElementSibling.classList.add('ativo');
+    accordionLista.forEach(item => {
+        item.addEventListener('click', ativarArccodion);
+    });
 }
 
-accordionLita.forEach(item => {
-    item.addEventListener('click', ativarArccodion)
-})
+//Ativando funções
+navegacaoTab();
+accordionNav();
