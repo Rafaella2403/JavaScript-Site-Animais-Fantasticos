6e6d44c22580576ea1e647fbab973d5493bf3994
Rafaella2403 <rribeirolimabritto@gmail.com>
2022-04-01 17:54:31 -0300

    Navegação por tab - CSS

--- script.js
+++ script.js
@@ -2,16 +2,22 @@
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabDescricao = document.querySelectorAll('.js-tabdescricao section');
 
-//Função responsável por adicionar a classe ativo ao elemento selecionar e remover dos demais elementos
-ativarTab = function(index){
-    tabDescricao.forEach(section => {
-        section.classList.remove('ativo');
-    });
-    tabDescricao[index].classList.add('ativo');
-}
+//Verificando se os itens existe na tela
 
-tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-        ativarTab(index);
-    })
-});
+if(tabMenu.length > 0 && tabDescricao.length > 0){
+    tabDescricao[0].classList.add('ativo');
+
+    //Função responsável por adicionar a classe ativo ao elemento selecionar e remover dos demais elementos
+    ativarTab = function(index){
+        tabDescricao.forEach(section => {
+            section.classList.remove('ativo');
+        });
+        tabDescricao[index].classList.add('ativo');
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+            ativarTab(index);
+        })
+    });
+}
